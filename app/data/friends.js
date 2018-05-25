d824ec9743a41cd3f4387913525b11e8a17fe080
dolphinman <wpfan.org@gmail.com>
2018-05-24 22:29:23 -0500

    updated js files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,30 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the waitlist tables.
-// Initially we just set it equal to a "dummy" customer.
+// Below data will hold all of the friend tables.
+// Initially we just set it equal to a "dummy" friend.
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var waitingArray = [
-    {
-      customerName: "Saima",
-      customerEmail: "saima@example.com",
-      phoneNumber: "000-000-0000",
-      customerID: "saimaCool"
-    }
+var friendArray = [
+  {
+    "name":"Steve",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
   ];
   
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = waitingArray;
+  module.exports = friendArray;
   
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   app.get("/reserve", function(req, res) {
