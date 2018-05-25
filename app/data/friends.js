b33af0346b95114ee5103fa2b357249d00c45db1
dolphinman <wpfan.org@gmail.com>
2018-05-25 15:13:33 -0500

    updaed api files

--- app/data/friends.js
+++ app/data/friends.js
@@ -22,6 +22,7 @@ var friendArray = [
         1
       ]
   }
+
   ];
   
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var friendData = require("../data/friends");
 
 
 // ===============================================================================
@@ -19,34 +18,28 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // Then the server saves the data to the friendData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    
+      friendData.push(req.body);
       res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    
+
   });
 
   // ---------------------------------------------------------------------------
@@ -55,9 +48,8 @@ module.exports = function(app) {
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    friendData = [];
 
-    console.log(tableData);
+    console.log(friendData);
   });
 };
