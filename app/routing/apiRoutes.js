962e51a2bda291c50df85a7b135a546686dad01c
dolphinman <wpfan.org@gmail.com>
2018-05-25 15:27:04 -0500

    updaed api files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on friendsData
 // ===============================================================================
 
 var friendData = require("../data/friends");
@@ -27,12 +27,12 @@ module.exports = function(app) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // (ex. User fills out a friend request... this data is then sent to the server...
   // Then the server saves the data to the friendData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // Note the code here. Our "server" will respond to requests and let users know if they have a friend match.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
     
